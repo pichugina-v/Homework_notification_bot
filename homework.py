28829128a1b9ac6a84c2938b95edf0bb8eacddae
Пичугина Валерия <Psyde26@yandex.ru>
2021-05-01 13:25:40 +0300

    info for requestexception added

--- homework.py
+++ homework.py
@@ -50,7 +50,7 @@ def get_homework_statuses(current_timestamp):
         )
         return homework_statuses.json()
     except requests.RequestException as e:
-        logging.error(f'Ошибка запроса: {e.__class__.__name__}: {e}')
+        logging.error(f'Ошибка запроса: {e}', exc_info=True)
         return {}
 
 
